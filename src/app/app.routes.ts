acb4d466ea2c73ad654480e989d69b63f6c62e24
Anup-Naik <Anupnaik54810@gmail.com>
2025-04-30 00:03:55 +0530

    Login Form 📃

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { SignupComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  { path: '/signup', component: SignupComponent },
-  { path: '/login', component: LoginComponent },
+  { path: '', pathMatch: 'full', component: HomeComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
 ];
