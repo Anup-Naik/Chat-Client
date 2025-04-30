c73631582b0ab3885be571d93c00413c487e5de4
Anup-Naik <Anupnaik54810@gmail.com>
2025-04-30 23:10:24 +0530

    Styles Update 🦦

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   template: `
-    <section class="home"></section>
+    <section class="home">
+      <div class="container">
+        <h1 class="head">Welcome To HiChat ! <br />Don't Stop Interacting</h1>
+        <article class="actions">
+          <a class="start" routerLink="/signup">Start Chatting</a>
+          <p class="subtext">
+            Already have an Account&quest; <a routerLink="/login">login</a>
+          </p>
+        </article>
+      </div>
+    </section>
   `,
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
